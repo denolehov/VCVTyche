d08c4559d107ee875ed3956ee40b879641b0196e
denolehov <denolehov@gmail.com>
2024-07-31 22:47:56 +0200

    Add Pythia implementation

--- src/Pythia.cpp
+++ src/Pythia.cpp
@@ -114,13 +114,60 @@ struct Pythia final : DaisyExpander {
 		LIGHTS_LEN
 	};
 
+	double phase = 0;
+	float variant = 1;
+
+	dsp::SchmittTrigger resetTrigger;
+	dsp::SchmittTrigger sampleAndHoldTrigger;
+
+	const float minSpeed = 0.001f;
+	const float maxSpeed = dsp::FREQ_A4;
+
+	float heldNoiseValue = 0.f;
+
 	Pythia() {
 		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
-		configParam(SPEED_PARAM, 0.01f, 1.f, 0.01f, "Speed");
+		configParam(SPEED_PARAM, 0.f, 1.f, 0.5f, "Speed");
 		configParam(VARIANT_PARAM, 1.f, 128.f, 1.f, "Variant");
 		configInput(HOLD_INPUT, "Sample and hold");
 		configInput(RESET_INPUT, "Reset");
 		configOutput(OUT_OUTPUT, "Main");
+	}
+
+	void process(const ProcessArgs& args) override
+	{
+		DaisyExpander::process(args);
+
+		if (resetTrigger.process(getInput(RESET_INPUT).getVoltage()))
+			reset();
+
+		// TODO: Provide a visual feedback about the fact that the variant has changed and now we're waiting for a reset.
+		const float newVariant = getParam(VARIANT_PARAM).getValue();
+		if (variant != newVariant && phase == 0)
+			variant = newVariant;
+
+		float outCV = heldNoiseValue;
+		if (getInput(HOLD_INPUT).isConnected()) {
+			// TODO: Add slew limiter to deal with spikes when the input has been just connected.
+			if (sampleAndHoldTrigger.process(getInput(HOLD_INPUT).getVoltage())) {
+				heldNoiseValue = outCV = noise->eval(variant, phase);
+			}
+		} else {
+			outCV = noise->eval(variant, phase);
+		}
+
+		outCV = rescale(outCV, -1.f, 1.f, -5.f, 5.f);
+
+		if (getOutput(OUT_OUTPUT).isConnected())
+			getOutput(OUT_OUTPUT).setVoltage(outCV);
+
+		const float speed = minSpeed * std::pow(maxSpeed / minSpeed, getParam(SPEED_PARAM).getValue());
+		phase += speed * args.sampleTime;
+	}
+
+	void reset() override
+	{
+		phase = 0;
 	}
 };
 
